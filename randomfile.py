63f1d1913b070e1c40c44a75e0cf7abd6ecf5ccb
Qays12 <qays_786@hotmail.co.uk>
2022-12-07 17:54:46 +0000

    Create randomfile.py

---
+++ randomfile.py
@@ -0,0 +1,19 @@
+import os
+import random
+import shutil
+import glob
+
+source = input("Enter the source directory: ")
+dest = input("Enter the Destination directory: ")
+
+pathchooser = rf'{source}'
+files = os.listdir(pathchooser)
+randomfile = random.choice(files)
+
+dir_path = rf'{dest}\**/'
+file_path = glob.glob(dir_path, recursive=True)
+randomdir = random.choice(file_path)
+
+path = rf'{source}\{randomfile}'
+sendpath = rf'{randomdir}/{randomfile}'
+shutil.copy(path, sendpath)
